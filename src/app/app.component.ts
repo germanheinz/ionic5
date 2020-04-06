302455721ec7469e809bfc50424adbd56ccea0fc
german <heinz.german@gmail.com>
2020-04-06 21:41:13 +0200

    Split-Pane Components -

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Observable } from 'rxjs';
+import { Components } from './interfaces/interfaces';
+import { ServiceService } from './services/service.service';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +13,19 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  componentes: Observable<Components[]>;
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private dataService: ServiceService
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      this.componentes = this.dataService.getMenuOpts();
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
